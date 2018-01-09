a9a5e7e6ce4ff41a417f9a2e429344f63345410d
felipequintella <felipequintella86@gmail.com>
2018-01-09 21:05:20 -0200

    prints key

--- functional_tests/management/commands/create_session.py
+++ functional_tests/management/commands/create_session.py
@@ -23,5 +23,5 @@ def create_pre_authenticated_session(email):
     session[SESSION_KEY] = user.pk
     session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
     session.save()
-    print("Saved key:" + session.session_key)
+    print(session.session_key)
     return session.session_key
