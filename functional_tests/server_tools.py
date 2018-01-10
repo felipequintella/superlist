f781c2d02c3b235eaab5ab237df03b6bb7ad486e
felipequintella <felipequintella86@gmail.com>
2018-01-09 22:33:52 -0200

    prints key

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -16,6 +16,4 @@ def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
     with settings(host_string=f'felipequintella@{host}'):
         session_key = run(f'{manage_dot_py} create_session {email}')
-        print(session_key)
-        print(session_key.strip())
         return session_key.strip()
